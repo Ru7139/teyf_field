a1ffff29670d1def4e61d00c48728172c250183c
Ru7139 <166471914+Ru7139@users.noreply.github.com>
2025-05-21 21:08:22 +0800

    Update main_block.rs

--- src/course/mike_code/surrealdb_tutorial/surrealdb_basic/main_block.rs
+++ src/course/mike_code/surrealdb_tutorial/surrealdb_basic/main_block.rs
@@ -1,5 +1,6 @@
 mod project {
-    use surrealdb::{engine::remote::ws::Ws, opt::auth::Root};
+    use serde::{Deserialize, Serialize};
+    use surrealdb::{RecordId, engine::remote::ws::Ws, opt::auth::Root};
 
     use super::super::db;
 
@@ -19,6 +20,72 @@ mod project {
 
         db.use_ns("alpha_ns").use_db("alpha_db").await?;
 
+        let record1: Option<Record> = db
+            .create("user")
+            .content(Person {
+                name: "Jonmo".into(),
+                age: 28u32,
+            })
+            .await?;
+
+        let record2: Option<Record> = db
+            .create("user")
+            .content(Person {
+                name: "Yacci".into(),
+                age: 381u32,
+            })
+            .await?;
+
+        dbg!(&record1.as_ref().unwrap().id);
+        dbg!(&record2.as_ref().unwrap().id);
+
+        let person: Vec<Option<User>> = db.select("user").await?;
+        person.iter().for_each(|x| println!("{:?}", x));
+
+        let person1: Option<User> = db
+            .select(("user", record1.as_ref().unwrap().id.key().to_string()))
+            .await?;
+        dbg!(&person1);
+
+        let person_deleted: Option<User> = db
+            .delete(("user", record1.as_ref().unwrap().id.key().to_string()))
+            .await?;
+        dbg!("person deleted", &person_deleted);
+
+        let person_update: Option<User> = db
+            .update(("user", record2.as_ref().unwrap().id.key().to_string()))
+            .content(User {
+                name: "Anne".into(),
+                age: 10u32,
+                id: RecordId::from(record2.as_ref().unwrap().id.clone()),
+            })
+            .await?;
+        dbg!("person updated", &person_update);
+
+        let person: Vec<Option<User>> = db.select("user").await?;
+        person.iter().for_each(|x| println!("{:?}", x));
+
+        // let _ = child.kill();
+        // dbg!("command line offline");
+
         Ok(())
+    }
+
+    #[derive(Debug, Serialize, Deserialize)]
+    struct Person {
+        name: String,
+        age: u32,
+    }
+
+    #[derive(Debug, Serialize, Deserialize, Clone)]
+    struct Record {
+        id: RecordId,
+    }
+
+    #[derive(Debug, Serialize, Deserialize)]
+    struct User {
+        id: RecordId,
+        name: String,
+        age: u32,
     }
 }
