a57524d44c9e0478f60cd658db16aecef158847b
Ru7139 <166471914+Ru7139@users.noreply.github.com>
2025-05-21 15:42:08 +0800

    some tries

--- src/course/mike_code/some_crates_tutorial/plotters_tur/plotters_chart.rs
+++ src/course/mike_code/some_crates_tutorial/plotters_tur/plotters_chart.rs
@@ -12,22 +12,22 @@ mod project {
 
         let mut chart = ChartBuilder::on(&root)
             .caption("Test", ("sans-serif", 50).into_font())
-            .margin(30)
-            .x_label_area_size(20)
-            .y_label_area_size(20)
+            .margin(100)
+            .x_label_area_size(30)
+            .y_label_area_size(30)
             .build_cartesian_2d(-3f32..3f32, -3f32..3f32)?;
 
         chart.configure_mesh().draw()?;
 
         chart
             .draw_series(LineSeries::new(
-                (-1i32..=1i32).map(|x| x as f32).map(|x| (x, x)),
+                (-1i32..=1i32).map(|x| x as f32).map(|x| (x, 2f32 * x)),
                 // (-1f32..=1f32).map(|x| (x,x))
                 // RangeInclusive<f32>` is not an iterator
                 &RED,
             ))?
-            .label("y = x")
-            .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 10, y)], &RED));
+            .label("y = 2x")
+            .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 30, y)], &RED));
 
         chart
             .configure_series_labels()
