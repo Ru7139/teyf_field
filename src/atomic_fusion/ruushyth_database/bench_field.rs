001820c2d99e7184f7df61a284f2ecb0866787a9
Ru7139 <166471914+Ru7139@users.noreply.github.com>
2025-05-28 13:54:21 +0800

    Update bench_field.rs

--- src/atomic_fusion/ruushyth_database/bench_field.rs
+++ src/atomic_fusion/ruushyth_database/bench_field.rs
@@ -76,6 +76,7 @@ async fn sdb_test() -> Result<(), Box<dyn std::error::Error>> {
 const TUSHARE_URL: &str = "http://api.tushare.pro";
 const DAILY_API: &str = "daily";
 const TOKEN_RU: &str = "e1c23bbb77f2cc2ae0169d5f6da2b5b0df3b685763dad71085559c5a";
+const TOKEN_FE: &str = "7ec7fdbb1c5d4c384becfdc5bcc0df6932503ea1a858dbf02196dabb";
 const NORMAL_FIELDS: &str =
     "ts_code, trade_date ,open, high, low, ,close, change, pct_chg, vol, amount";
 
