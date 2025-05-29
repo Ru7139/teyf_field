91d07f836f362d2aa24e06997ee4431d1f854a92
Ru7139 <166471914+Ru7139@users.noreply.github.com>
2025-05-29 16:31:23 +0800

    Update auto_button_block.rs

--- src/atomic_fusion/ruushyth_database/auto_button_block.rs
+++ src/atomic_fusion/ruushyth_database/auto_button_block.rs
@@ -1,7 +1,7 @@
 #[tokio::test]
 async fn launch_bomb() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let year = 2021;
-    let folder = "src/atomic_fusion/ruushyth_database/workshop/raw_stock_file/{}";
+    let folder = "src/atomic_fusion/ruushyth_database/workshop/raw_stock_file/";
     let download_folder = format!("{}/{}", folder, year);
 
     super::solution_0::http_fetch_tushare_year_dayk_use_ru_token(year, &download_folder).await?;
