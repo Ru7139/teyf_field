4ea31d3b86b717ce3c9b96d581fad0e31f890767
Ru7139 <166471914+Ru7139@users.noreply.github.com>
2025-06-06 21:34:40 +0800

    Update basic_actix.rs

--- src/course/mike_code/actix_web_turtorial/basic_actix.rs
+++ src/course/mike_code/actix_web_turtorial/basic_actix.rs
@@ -5,10 +5,16 @@ mod project {
     async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         let _side_running_server = tokio::spawn(async move {
             HttpServer::new(|| {
-                App::new().route(
-                    "/",
-                    web::get().to(|| async { HttpResponse::Ok().body("Hello World".to_string()) }),
-                )
+                App::new()
+                    .route(
+                        "/helloworld",
+                        web::get()
+                            .to(|| async { HttpResponse::Ok().body("Hello World".to_string()) }),
+                    )
+                    .route(
+                        "/hello",
+                        web::get().to(|| async { HttpResponse::Ok().body("Hello".to_string()) }),
+                    )
             })
             .bind("127.0.0.1:65534")
             .unwrap()
