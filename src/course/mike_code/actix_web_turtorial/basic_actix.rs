fee907b878b376a310319644b0bdebe9817f7f54
Ru7139 <166471914+Ru7139@users.noreply.github.com>
2025-06-06 21:30:07 +0800

    add actix-web 01

--- src/course/mike_code/actix_web_turtorial/basic_actix.rs
+++ src/course/mike_code/actix_web_turtorial/basic_actix.rs
@@ -1,6 +1,22 @@
 mod project {
-    #[tokio::test]
+    use actix_web::{App, HttpResponse, HttpServer, web};
+
+    #[actix_web::test]
     async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+        let _side_running_server = tokio::spawn(async move {
+            HttpServer::new(|| {
+                App::new().route(
+                    "/",
+                    web::get().to(|| async { HttpResponse::Ok().body("Hello World".to_string()) }),
+                )
+            })
+            .bind("127.0.0.1:65534")
+            .unwrap()
+            .run()
+            .await
+            .unwrap()
+        });
+
         Ok(())
     }
 }
