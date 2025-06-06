79d06752661583041a698afc8e096a27f8bec655
Ru7139 <166471914+Ru7139@users.noreply.github.com>
2025-06-06 21:10:49 +0800

    Add actix turtorial

---
+++ src/course/mike_code/actix_web_turtorial/basic_actix.rs
@@ -0,0 +1,6 @@
+mod project {
+    #[tokio::test]
+    async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+        Ok(())
+    }
+}
--- src/course/mike_code/mod.rs
+++ src/course/mike_code/mod.rs
@@ -1,3 +1,4 @@
+pub mod actix_web_turtorial;
 pub mod axum_tutorial;
 pub mod dioxus_turtorial;
 pub mod some_crates_tutorial;
