3b9dc9ac6c46db75be67e8a6522e898ffc23f596
TongaEast <166471914+Ru7139@users.noreply.github.com>
2025-06-07 23:05:14 +0800

    actix-web 04

--- src/course/mike_code/actix_web_turtorial/basic_actix.rs
+++ src/course/mike_code/actix_web_turtorial/basic_actix.rs
@@ -1,17 +1,20 @@
 mod project {
-    use actix_web::{App, HttpResponse, HttpServer, web};
+    use actix_web::{App, HttpResponse, HttpServer, Responder, web};
 
     #[actix_web::test]
     async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         let _side_running_server = tokio::spawn(async move {
             HttpServer::new(|| {
-                App::new().service(
-                    web::resource("/method")
-                        .route(web::get().to(|| async { HttpResponse::Ok().body("Get") }))
-                        .route(web::post().to(|| async { HttpResponse::Ok().body("Post") }))
-                        .route(web::delete().to(|| async { HttpResponse::Ok().body("Delete") }))
-                        .route(web::put().to(|| async { HttpResponse::Ok().body("Put") })),
-                )
+                App::new()
+                    .service(
+                        web::resource("/method")
+                            .route(web::get().to(|| async { HttpResponse::Ok().body("Get") }))
+                            .route(web::post().to(|| async { HttpResponse::Ok().body("Post") }))
+                            .route(web::delete().to(|| async { HttpResponse::Ok().body("Delete") }))
+                            .route(web::put().to(|| async { HttpResponse::Ok().body("Put") })),
+                    )
+                    .service(hello)
+                    .service(world)
             })
             .bind("127.0.0.1:65534")
             .unwrap()
@@ -20,7 +23,25 @@ mod project {
             .unwrap()
         });
 
+        let rqs_client = reqwest::Client::new();
+        let rqs = rqs_client
+            .get("http://127.0.0.1:65534/hello")
+            .send()
+            .await?;
+
+        assert_eq!(rqs.text().await?, "hello");
+
         // _side_running_server.await?;
         Ok(())
+    }
+
+    #[actix_web::get("/hello")]
+    async fn hello() -> impl Responder {
+        HttpResponse::Ok().body("hello")
+    }
+
+    #[actix_web::get("/world")]
+    async fn world() -> impl Responder {
+        HttpResponse::Ok().body("world")
     }
 }
