2940560ab220f540326ecc7ae1e6d400839ab3f9
Ru7139 <166471914+Ru7139@users.noreply.github.com>
2025-07-14 21:58:24 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
+use actix_web::{HttpResponse, Responder};
+
 mod course;
 mod metextbook;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    actix_web::HttpServer::new(move || actix_web::App::new().service(hello))
+        .bind("127.0.0.1:65534")?
+        .run()
+        .await?;
+
     Ok(())
+}
+
+#[actix_web::get("/hello")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("hello")
 }
